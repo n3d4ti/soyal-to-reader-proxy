ffbf7292a6ed181d7f8e23f1dba0b0a69341422f
n3d4ti <40421727+n3d4ti@users.noreply.github.com>
2020-10-15 13:55:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -55,7 +55,7 @@ def client(cmd):
 @app.route('/', methods=['GET'])
 def home():
     return '''<h5>API endpoint:</h5>
-<p>[GET] /api/v1/command    --  get all events</p>
+<p>[GET] /api/v1/command    --  send a command</p>
 <p>Sample: curl http://192.168.12.34/api/v1/command?cmd=7e0433ab2112ab0e</p>'''
 
 @app.route('/api/v1/command', methods=['GET'])
